7e887e97f4965d95774a6f41e99d5beb290d7c24
safuh <sarayji>
2018-11-28 14:06:23 -0800

    Write render method for toggle class

--- ajaxtwitter/frontend/follow_toggle.js
+++ ajaxtwitter/frontend/follow_toggle.js
@@ -4,6 +4,16 @@ class FollowToggle {
     this.userId = this.$el.data('user-id') || options.userId;
     this.followState = (this.$el.data('2') || options.followState);
   }
+  
+  render () {
+    if (this.followState === 'unfollowed') {
+      this.$el.prop('disabled', false); // Add a property of disabled and set value to false
+      this.$el.html('Follow!');
+    }
+    else if (this.followState === 'followed')
+    this.$el.prop('disabled', false);
+    this.$el.html('Unfollow!');
+  }
 }
 
 module.exports = FollowToggle;
